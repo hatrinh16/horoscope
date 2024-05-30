7287db7c4018db49862312a47e44a9f8c680cf0f
hatrinh16 <hatrinh@boomco.org>
2024-05-30 14:58:14 +0700

    change favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }: any) {
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
           />
-          <link rel="shortcut icon" href="/favicon.webp" />
+          <link rel="shortcut icon" href="/fav.png" />
         </Head>
         <Component {...pageProps} />
       </AppProvider>
