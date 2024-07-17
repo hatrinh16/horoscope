e815b34254b36ac0057432e640d8af73a7facf2d
hatrinh16 <hatrinh@boomco.org>
2024-07-17 12:01:02 +0700

    search console

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }: any) {
       <AppProvider>
         <Head>
           <title>Daily Horoscope | DailyAstro</title>
+          <meta name="google-site-verification" content="Z7Xj1mON6jtmxhfHYq1IjUEfdly8pa49_OLX_yFl0QA" />
           <meta
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
